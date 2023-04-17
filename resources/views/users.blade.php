7134d37285c1d8d2fdc473868619f0e21ed730f5
elfanhari <elfanhari88@gmail.com>
2023-04-17 10:27:43 +0700

    perubahan

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -60,7 +60,7 @@
             <div class="row">
                 <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
                     <h2>
-                        Silahkan masukkan NISN (Nomor Induk Siswa Nasional) untuk mengecek informasi
+                        Silahkan masukkan NISN (Nomor Induk Siswa Nasional) untuk mengecek informasi ya!
                     </h2>
                     <div class="form-group mt-3">
                         @csrf
